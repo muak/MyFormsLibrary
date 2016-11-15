47353bc69ed1b695b2999d610c47abd788744fc1
kamu <kamuxxx@hotmail.com>
2016-11-15 16:32:03 +0900

    Update Ver.1.1.0-pre2 AddErrorMessageEffect追加

---
+++ MyFormsLibrary.Droid/Effects/AddErrorMessagePlatformEffect.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+using MyFormsLibrary.Droid.Effects;
+using MyFormsLibrary.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AGraphics = Android.Graphics;
+
+[assembly: ExportEffect(typeof(AddErrorMessagePlatformEffect), nameof(AddErrorMessage))]
+namespace MyFormsLibrary.Droid.Effects
+{
+    public class AddErrorMessagePlatformEffect:PlatformEffect
+    {
+        private TextView textView;
+
+        protected override void OnAttached() {
+            textView = new TextView(Container.Context);
+            textView.SetTextColor(new AGraphics.Color(255, 0, 0, 200));
+            textView.SetBackgroundColor(new AGraphics.Color(255, 255, 255, 128));
+            textView.TextSize = 10f;
+            textView.Gravity = GravityFlags.Left | GravityFlags.CenterVertical;
+            textView.LayoutParameters = new ViewGroup.LayoutParams(-1, -1);
+            textView.Visibility = ViewStates.Invisible;
+            textView.SetPadding(8, 0, 8, 0);
+            Container.AddView(textView);
+
+            UpdateText();
+
+        }
+
+        protected override void OnDetached() {
+            textView.Dispose();
+        }
+
+
+        private void UpdateText() {
+            var msg = AddErrorMessage.GetErrorMessage(Element);
+            if (string.IsNullOrEmpty(msg)) {
+                textView.Visibility = ViewStates.Invisible;
+                return;
+            }
+
+            textView.Text = msg;
+
+            var textpaint = textView.Paint;
+            var rect = new Android.Graphics.Rect();
+            textpaint.GetTextBounds(msg, 0, msg.Length, rect);
+            var addwidth = (int)(rect.Width() / msg.Length) + textView.PaddingLeft + textView.PaddingRight;
+            var addHeight = rect.Height() / 2;
+            var left = Control.Right - rect.Width() - addwidth > 0 ? Control.Right - rect.Width() - addwidth : 0;
+            var height = (rect.Width() / Control.Right + 1) * rect.Height() + addHeight;
+
+            textView.Top = 0;
+            textView.Left = left;
+            textView.Right = Control.Right;
+            textView.Bottom = height;
+
+            textView.Visibility = ViewStates.Visible;
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged(e);
+
+            if (e.PropertyName == AddErrorMessage.ErrorMessageProperty.PropertyName) {
+                UpdateText();
+            }
+
+        }
+    }
+}
---
+++ MyFormsLibrary.iOS/Effects/AddErrorMessagePlatformEffect.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using MyFormsLibrary.Effects;
+using MyFormsLibrary.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportEffect(typeof(AddErrorMessagePlatformEffect), nameof(AddErrorMessage))]
+namespace MyFormsLibrary.iOS.Effects
+{
+    public class AddErrorMessagePlatformEffect:PlatformEffect
+    {
+        private UILabel errorLabel;
+        private NSLayoutConstraint[] constraint;
+
+        protected override void OnAttached() {
+            errorLabel = new UILabel();
+            errorLabel.LineBreakMode = UILineBreakMode.Clip;
+            errorLabel.Lines = 1;
+            errorLabel.BackgroundColor = UIColor.FromWhiteAlpha(1, .5f);
+            errorLabel.TextColor = UIColor.Red.ColorWithAlpha(0.8f);
+            errorLabel.TintAdjustmentMode = UIViewTintAdjustmentMode.Automatic;
+            errorLabel.AdjustsFontSizeToFitWidth = true;
+            errorLabel.BaselineAdjustment = UIBaselineAdjustment.AlignCenters;
+            errorLabel.TextAlignment = UITextAlignment.Center;
+            errorLabel.AdjustsLetterSpacingToFitWidth = true;
+            errorLabel.Font = errorLabel.Font.WithSize(10);
+
+            UpdateText();
+
+            Container.AddSubview(errorLabel);
+
+            errorLabel.TranslatesAutoresizingMaskIntoConstraints = false;
+            constraint = new NSLayoutConstraint[]{
+                NSLayoutConstraint.Create(
+                    errorLabel,
+                    NSLayoutAttribute.Top,
+                    NSLayoutRelation.Equal,
+                    Container,
+                    NSLayoutAttribute.Top,
+                    1,
+                    0
+                ),
+                NSLayoutConstraint.Create(
+                    errorLabel,
+                    NSLayoutAttribute.Right,
+                    NSLayoutRelation.Equal,
+                    Container,
+                    NSLayoutAttribute.Right,
+                    1,
+                    0
+                ),
+                NSLayoutConstraint.Create(
+                    errorLabel,
+                    NSLayoutAttribute.Height,
+                    NSLayoutRelation.Equal,
+                    null,
+                    NSLayoutAttribute.Height,
+                    1,
+                    14
+                )
+            };
+
+            Container.AddConstraints(constraint);
+            errorLabel.SizeToFit();
+
+        }
+
+        protected override void OnDetached() {
+            Control.RemoveConstraints(constraint);
+            errorLabel.RemoveFromSuperview();
+            errorLabel.Dispose();
+        }
+
+        private void UpdateText() {
+            var msg = AddErrorMessage.GetErrorMessage(Element);
+            errorLabel.Text = $" {msg} ";
+            errorLabel.Hidden = string.IsNullOrEmpty(msg);
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged(e);
+
+            if (e.PropertyName == AddErrorMessage.ErrorMessageProperty.PropertyName) {
+                UpdateText();
+            }
+
+        }
+    }
+}
