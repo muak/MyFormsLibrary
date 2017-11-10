a738a72737c349c5c4dc60cbec62455356ea7994
kamu <kamuxxx@hotmail.com>
2017-11-10 17:59:41 +0900

    iOS ListView バグ対応 ブランチ切るの忘れた！

--- MyFormsLibrary.iOS/CustomRenderers/NonSelectionListViewRenderer.cs
+++ MyFormsLibrary.iOS/CustomRenderers/NonSelectionListViewRenderer.cs
@@ -3,7 +3,12 @@ using MyFormsLibrary.CustomRenderers;
 using MyFormsLibrary.iOS.CustomRenderers;
 using UIKit;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Platform.iOS;
+using CoreAnimation;
+using System;
+using System.Collections.Specialized;
+using System.Reflection;
 
 [assembly: ExportRenderer(typeof(NonSelectionListView), typeof(NonSelectionListViewRenderer))]
 namespace MyFormsLibrary.iOS.CustomRenderers
@@ -11,8 +16,9 @@ namespace MyFormsLibrary.iOS.CustomRenderers
 	public class NonSelectionListViewRenderer:ListViewRenderer,IUITableViewDelegate
 	{
 		public List<NonSelectionViewCellRenderer> ItemCleanUp { get; set; }
-
+       
 		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ListView> e) {
+
 			base.OnElementChanged(e);
 
 			if (e.NewElement != null) {
@@ -20,10 +26,26 @@ namespace MyFormsLibrary.iOS.CustomRenderers
 				//Control.Bounces = false;
 				Control.AllowsSelection = false;
                 ItemCleanUp = new List<NonSelectionViewCellRenderer>();
+
+                var mCollectionChanged = typeof(ListViewRenderer).GetMethod("OnCollectionChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+               
+                Action<ITemplatedItemsList<Cell>> removeEvent = (cell) => {
+                    Delegate eventMethod = mCollectionChanged.CreateDelegate(typeof(NotifyCollectionChangedEventHandler), this);
+                    cell.GetType().GetEvent("CollectionChanged").RemoveEventHandler(cell, eventMethod);
+                };
+
+                var templatedItems = ((ITemplatedItemsView<Cell>)e.NewElement).TemplatedItems;
+                removeEvent(templatedItems);
+                templatedItems.CollectionChanged += TemplatedItems_CollectionChanged;
+
 			}
 		}
 
 		protected override void Dispose(bool disposing) {
+            if(disposing){
+                var templatedItems = ((ITemplatedItemsView<Cell>)Element).TemplatedItems;
+                templatedItems.CollectionChanged -= TemplatedItems_CollectionChanged;
+            }
 			foreach (var d in ItemCleanUp) {
 				d.CleanUp();
 			}
@@ -34,8 +56,14 @@ namespace MyFormsLibrary.iOS.CustomRenderers
 		}
 
 
+        void TemplatedItems_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Control.ReloadData();          
+        }
 
 
-	}
+
+
+    }
 }
 
