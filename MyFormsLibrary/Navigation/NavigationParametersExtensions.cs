d68ecbdf5057b4a626c65e7369fdc362fbdec54f
kamu <kamuxxx@hotmail.com>
2017-11-14 17:37:37 +0900

    NavigationParameter変換クラス追加

---
+++ MyFormsLibrary/Navigation/NavigationParametersExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace MyFormsLibrary.Navigation
+{
+    public static class NavigationParametersExtensions
+    {
+        /// <summary>
+        /// NavigationParametersを各画面のParametersに変換する
+        /// </summary>
+        /// <returns>The to.</returns>
+        /// <param name="param">Parameter.</param>
+        /// <typeparam name="T">変換する画面パラメータの型を指定</typeparam>
+        public static T To<T>(this NavigationParameters param) where T : ParametersBase, new() {
+            return param[ParametersBase.ParameterKey] as T;
+        }
+    }
+}
---
+++ MyFormsLibrary/Navigation/ParametersBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace MyFormsLibrary.Navigation
+{
+    public abstract class ParametersBase
+    {
+        internal const string ParameterKey = "kamusoft";
+        /// <summary>
+        /// 画面パラメータをPrism.Navigation.NavigationParametersに変換する
+        /// </summary>
+        /// <returns>The navigation parameters.</returns>
+        public NavigationParameters ToNavigationParameters() {
+            return new NavigationParameters{
+                {ParameterKey,this}
+            };
+        }
+    }
+}
